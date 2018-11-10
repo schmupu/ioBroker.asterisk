3f23b368ef91a9d1514f1ae27d06804744b57a52
Thorsten Stueben <thorsten.stueben@macbook.local>
2018-11-10 21:45:33 +0100

    Update

--- asterisk.js
+++ asterisk.js
@@ -4,6 +4,11 @@ var utils = require(__dirname + '/lib/utils'); // Get common adapter utils
 var dp = require(__dirname + '/lib/datapoints');
 var net = require('net');
 var adapter = new utils.Adapter('asterisk');
+var ami = require(__dirname + '/lib/ami');
+var transcode = require(__dirname + '/lib/transcode');
+var asterisk = undefined
+var converter = undefined;
+
 
 // *****************************************************************************************************
 // is called when adapter shuts down - callback has to be called under any circumstances!
@@ -24,7 +29,37 @@ adapter.on('unload', (callback) => {
 
 });
 
- 
+
+adapter.on('message', (msg) => {
+  let command = msg.command;
+  let parameter = msg.message;
+  let id = msg._id;
+
+  adapter.log.debug('Message: ' + JSON.stringify(msg));
+
+  if (command == 'dial') {
+    if (parameter && parameter.text && parameter.telnr) {
+      if(!parameter.audiofile) parameter.audiofile = '/tmp/audio_' + id;
+      converter.textToGsm(parameter.text, 'DE', 100, parameter.audiofile + ".gsm")
+        .then((file) => {
+          adapter.log.info("finish" + JSON.stringify(file));
+          // The file is converted at path "file"
+          asterisk.dial(parameter, (err, res) => {
+            adapter.log.info(res);
+          });
+        })
+        .catch((err) => {
+          // An error occured
+          adapter.log.error(err);
+        });
+    } else {
+      adapter.log.error('Paramter telnr and/or text are missing');
+    }
+  }
+
+});
+
+
 
 
 // *****************************************************************************************************
@@ -43,6 +78,10 @@ adapter.on('ready', () => {
 // Main function
 // *****************************************************************************************************
 function main() {
+
+  asterisk = new ami(adapter.config.port, adapter.config.ip, adapter.config.user, adapter.config.password, false);
+  converter = new transcode();
+ 
 
 }
 
