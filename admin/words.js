624adcd48c77bfd8e42573c6546516cb93bdd6f8
ldittmar <ldittmar.privat@gmail.com>
2019-01-23 23:23:56 +0100

    Add gulp auto translation
    This pr add gulp auto translation and some missing translations.
    
    For informations about gulp automatic translation (german): https://forum.iobroker.net/viewtopic.php?f=24&t=21208#p222837

--- admin/words.js
+++ admin/words.js
@@ -1,126 +1,16 @@
 /*global systemDictionary:true */
-
+'use strict';
 
 systemDictionary = {
-  "on save adapter restarts with new config immediately": {
-    "en": "on save adapter restarts with new config immediately",
-    "de": "Beim Speichern wird der Adapter sofort mit der neuen Konfiguration gestartet",
-    "ru": "на перезаписываемом адаптере перезагружается с новой конфигурацией немедленно",
-    "pt": "on save adapter reinicia com nova configuração imediatamente",
-    "nl": "on save-adapter wordt onmiddellijk opnieuw opgestart met nieuwe config",
-    "fr": "sur save, l'adaptateur redémarre immédiatement avec la nouvelle configuration",
-    "it": "su save adapter si riavvia immediatamente con la nuova configurazione",
-    "es": "en guardar adaptador se reinicia con nueva configuración inmediatamente",
-    "pl": "na karcie save natychmiast uruchamia się z nową konfiguracją"
-  },
-  "Main settings": {
-    "en": "Main settings",
-    "de": "Haupteinstellungen",
-    "ru": "Основные настройки",
-    "pt": "Configurações principais",
-    "nl": "Belangrijkste instellingen",
-    "fr": "Réglages principaux",
-    "it": "Impostazioni principali",
-    "es": "Ajustes principales",
-    "pl": "Ustawienia główne"
-  },
-  "Port": {
-    "en": "Port",
-    "de": "Port",
-    "ru": "Port",
-    "pt": "Port",
-    "nl": "Port",
-    "fr": "Port",
-    "it": "Port",
-    "es": "Port",
-    "pl": "Port"
-  },
-  "IP": {
-    "en": "IP-Address",
-    "de": "IP Adresse",
-    "ru": "Айпи адрес",
-    "pt": "Endereço de IP",
-    "nl": "IP adres",
-    "fr": "Adresse IP",
-    "it": "Indirizzo IP",
-    "es": "Dirección IP",
-    "pl": "Adres IP"
-  },
-  "Username (Manager)": {
-    "en": "Username (Manager)",
-    "de": "Benutzername (Manager)",
-    "ru": "Имя пользователя (менеджер)",
-    "pt": "Nome de usuário (gerente)",
-    "nl": "Gebruikersnaam (manager)",
-    "fr": "Nom d'utilisateur (gestionnaire)",
-    "it": "Username (Manager)",
-    "es": "Nombre de usuario (administrador)",
-    "pl": "Nazwa użytkownika (menedżer)"
-  },
-  "Password (Manager)": {
-    "en": "Password (Manager)",
-    "de": "Passwort (Manager)",
-    "ru": "Пароль (Менеджер)",
-    "pt": "Senha (Manager)",
-    "nl": "Wachtwoord (Manager)",
-    "fr": "Mot de passe (gestionnaire)",
-    "it": "Password (Manager)",
-    "es": "Contraseña (Administrador)",
-    "pl": "Hasło (menedżer)"
-  },
-  "Path for temporary audio files. Must be accessible and authorized for Asterisk and ioBroker": {
-    "en": "Path for temporary audio files. Must be accessible and authorized for Asterisk and ioBroker",
-    "de": "Pfad für temporäre Audiodateien. Muss für Asterisk und ioBroker zugänglich und autorisiert sein",
-    "ru": "Путь для временных аудиофайлов. Должен быть доступен и разрешен для Asterisk и ioBroker",
-    "pt": "Caminho para arquivos de áudio temporários. Deve ser acessível e autorizado para o Asterisk e o ioBroker",
-    "nl": "Pad voor tijdelijke audiobestanden. Moet toegankelijk en geautoriseerd zijn voor Asterisk en ioBroker",
-    "fr": "Chemin pour les fichiers audio temporaires. Doit être accessible et autorisé pour Asterisk et ioBroker",
-    "it": "Percorso per file audio temporanei. Deve essere accessibile e autorizzato per Asterisk e ioBroker",
-    "es": "Ruta para archivos de audio temporales. Debe ser accesible y autorizado para Asterisk y ioBroker.",
-    "pl": "Ścieżka do tymczasowych plików audio. Musi być dostępny i autoryzowany dla Asterisk i ioBroker"
-  },
-  "Language": {
-    "en": "Language",
-    "de": "Sprache",
-    "ru": "язык",
-    "pt": "Língua",
-    "nl": "Taal",
-    "fr": "La langue",
-    "it": "linguaggio",
-    "es": "Idioma",
-    "pl": "Język"
-  },
-  "Audio language": {
-    "en": "Audio language",
-    "de": "Audiosprache",
-    "ru": "Аудио язык",
-    "pt": "Idioma de áudio",
-    "nl": "Audiotaal",
-    "fr": "Langue audio",
-    "it": "Linguaggio audio",
-    "es": "Idioma de audio",
-    "pl": "Język audio"
-  },
-  "Username in sip.conf or pjsip.conf": {
-    "en": "Username in sip.conf or pjsip.conf",
-    "de": "Benutzername in der sip.conf / pjsip.conf",
-    "ru": "Имя пользователя в sip.conf / pjsip.conf",
-    "pt": "Nome de usuário no sip.conf / pjsip.conf",
-    "nl": "Gebruikersnaam in sip.conf / pjsip.conf",
-    "fr": "Nom d'utilisateur dans sip.conf / pjsip.conf",
-    "it": "Nome utente in sip.conf / pjsip.conf",
-    "es": "Nombre de usuario en sip.conf / pjsip.conf",
-    "pl": "Nazwa użytkownika w pliku sip.conf / pjsip.conf"
-  },
-  "Please choose transcoder": {
-    "en": "Please choose transcoder",
-    "de": "Bitte wählen Sie den Transcoder",
-    "ru": "Пожалуйста, выберите транскодер",
-    "pt": "Por favor, escolha o transcodificador",
-    "nl": "Kies alstublieft transcoder",
-    "fr": "S'il vous plaît choisir transcodeur",
-    "it": "Si prega di scegliere il transcoder",
-    "es": "Por favor, elija el transcodificador",
-    "pl": "Wybierz transkoder"
-  }
+    "Audio language": {                              "en": "Audio language",                                  "de": "Audiosprache",                                    "ru": "Аудио язык",                                      "pt": "Idioma de áudio",                                 "nl": "Audiotaal",                                       "fr": "Langue audio",                                    "it": "Linguaggio audio",                                "es": "Idioma de audio",                                 "pl": "Język audio",                                     "zh-cn": "音频语言"},
+    "IP": {                                          "en": "IP-Address",                                      "de": "IP Adresse",                                      "ru": "Айпи адрес",                                      "pt": "Endereço de IP",                                  "nl": "IP adres",                                        "fr": "Adresse IP",                                      "it": "Indirizzo IP",                                    "es": "Dirección IP",                                    "pl": "Adres IP",                                        "zh-cn": "IP地址"},
+    "Language": {                                    "en": "Language",                                        "de": "Sprache",                                         "ru": "язык",                                            "pt": "Língua",                                          "nl": "Taal",                                            "fr": "La langue",                                       "it": "linguaggio",                                      "es": "Idioma",                                          "pl": "Język",                                           "zh-cn": "语言"},
+    "Main settings": {                               "en": "Main settings",                                   "de": "Haupteinstellungen",                              "ru": "Основные настройки",                              "pt": "Configurações principais",                        "nl": "Belangrijkste instellingen",                      "fr": "Réglages principaux",                             "it": "Impostazioni principali",                         "es": "Ajustes principales",                             "pl": "Ustawienia główne",                               "zh-cn": "主要设置"},
+    "Password (Manager)": {                          "en": "Password (Manager)",                              "de": "Passwort (Manager)",                              "ru": "Пароль (Менеджер)",                               "pt": "Senha (Manager)",                                 "nl": "Wachtwoord (Manager)",                            "fr": "Mot de passe (gestionnaire)",                     "it": "Password (Manager)",                              "es": "Contraseña (Administrador)",                      "pl": "Hasło (menedżer)",                                "zh-cn": "密码（经理）"},
+    "Path for temporary audio files. Must be accessible and authorized for Asterisk and ioBroker": {"en": "Path for temporary audio files. Must be accessible and authorized for Asterisk and ioBroker", "de": "Pfad für temporäre Audiodateien. Muss für Asterisk und ioBroker zugänglich und autorisiert sein", "ru": "Путь для временных аудиофайлов. Должен быть доступен и разрешен для Asterisk и ioBroker", "pt": "Caminho para arquivos de áudio temporários. Deve ser acessível e autorizado para o Asterisk e o ioBroker", "nl": "Pad voor tijdelijke audiobestanden. Moet toegankelijk en geautoriseerd zijn voor Asterisk en ioBroker", "fr": "Chemin pour les fichiers audio temporaires. Doit être accessible et autorisé pour Asterisk et ioBroker", "it": "Percorso per file audio temporanei. Deve essere accessibile e autorizzato per Asterisk e ioBroker", "es": "Ruta para archivos de audio temporales. Debe ser accesible y autorizado para Asterisk y ioBroker.", "pl": "Ścieżka do tymczasowych plików audio. Musi być dostępny i autoryzowany dla Asterisk i ioBroker", "zh-cn": "临时音频文件的路径。"},
+    "Please choose transcoder": {                    "en": "Please choose transcoder",                        "de": "Bitte wählen Sie den Transcoder",                 "ru": "Пожалуйста, выберите транскодер",                 "pt": "Por favor, escolha o transcodificador",           "nl": "Kies alstublieft transcoder",                     "fr": "S'il vous plaît choisir transcodeur",             "it": "Si prega di scegliere il transcoder",             "es": "Por favor, elija el transcodificador",            "pl": "Wybierz transkoder",                              "zh-cn": "请选择代码转换器"},
+    "Port": {                                        "en": "Port",                                            "de": "Port",                                            "ru": "порт",                                            "pt": "Porta",                                           "nl": "Haven",                                           "fr": "Port",                                            "it": "Porta",                                           "es": "Puerto",                                          "pl": "Port",                                            "zh-cn": "港口"},
+    "Username (Manager)": {                          "en": "Username (Manager)",                              "de": "Benutzername (Manager)",                          "ru": "Имя пользователя (менеджер)",                     "pt": "Nome de usuário (gerente)",                       "nl": "Gebruikersnaam (manager)",                        "fr": "Nom d'utilisateur (gestionnaire)",                "it": "Username (Manager)",                              "es": "Nombre de usuario (administrador)",               "pl": "Nazwa użytkownika (menedżer)",                    "zh-cn": "用户名（经理）"},
+    "Username in sip.conf or pjsip.conf": {          "en": "Username in sip.conf or pjsip.conf",              "de": "Benutzername in der sip.conf / pjsip.conf",       "ru": "Имя пользователя в sip.conf / pjsip.conf",        "pt": "Nome de usuário no sip.conf / pjsip.conf",        "nl": "Gebruikersnaam in sip.conf / pjsip.conf",         "fr": "Nom d'utilisateur dans sip.conf / pjsip.conf",    "it": "Nome utente in sip.conf / pjsip.conf",            "es": "Nombre de usuario en sip.conf / pjsip.conf",      "pl": "Nazwa użytkownika w pliku sip.conf / pjsip.conf", "zh-cn": "sip.conf或pjsip.conf中的用户名"},
+    "on save adapter restarts with new config immediately": {"en": "on save adapter restarts with new config immediately", "de": "Beim Speichern wird der Adapter sofort mit der neuen Konfiguration gestartet", "ru": "на перезаписываемом адаптере перезагружается с новой конфигурацией немедленно", "pt": "on save adapter reinicia com nova configuração imediatamente", "nl": "on save-adapter wordt onmiddellijk opnieuw opgestart met nieuwe config", "fr": "sur save, l'adaptateur redémarre immédiatement avec la nouvelle configuration", "it": "su save adapter si riavvia immediatamente con la nuova configurazione", "es": "en guardar adaptador se reinicia con nueva configuración inmediatamente", "pl": "na karcie save natychmiast uruchamia się z nową konfiguracją", "zh-cn": "保存适配器立即重新启动新配置"},
 };
